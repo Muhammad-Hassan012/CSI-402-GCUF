e99e7d2ee4bdf9271e89f7d81d92148059dff518
Muhammad-Hassan012 <mr.hassan174@gmail.com>
2024-08-24 02:40:49 -0700

    Your commit message

---
+++ pointers/file.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int  n =7 ;
+    cout << n <<endl;
+    cout << &n <<endl;
+    int *ptr = &n;
+    cout << ptr<<endl;
+    cout << ptr<<endl;
+    cout << ptr<<endl;
+    cout << *ptr<<endl;
+
+
+    return 0;
+}
