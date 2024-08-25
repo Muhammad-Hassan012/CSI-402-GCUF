0d39557e6f6d9f6fdbeb0d32452c2d0593469a3d
Muhammad-Hassan012 <mr.hassan174@gmail.com>
2024-08-25 11:32:51 -0700

    Updated file.cpp and file.exe

--- pointers/file.cpp
+++ pointers/file.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int  n =7 ;
-    cout << n <<endl;
-    cout << &n <<endl;
-    int *ptr = &n;
-    cout << ptr<<endl;
-    cout << ptr<<endl;
-    cout << ptr<<endl;
-    cout << *ptr<<endl;
-
+void printNumber(int* numberPtr) {
+cout<< *numberPtr <<endl;
+};
+void printletter(char* charPtr) {
+cout<< *charPtr <<endl;
+};
+int main()
+{
+    // int n = 7;
+    // cout << n << endl;
+    // cout << &n << endl;
+    // int *ptr = &n;
+    // cout << ptr << endl;
+    // cout << ptr << endl;
+    // cout << ptr << endl;
+    // cout << *ptr << endl;
+    ////////////////////////////////////////
+    // void pointers
+    int number = 5;
+    char letter = 'a';
+    printNumber(&number);
+    printletter(&letter);
 
     return 0;
 }
